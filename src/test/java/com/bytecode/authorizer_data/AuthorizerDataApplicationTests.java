3214e90d7c0d974bd65cf95079b11af590989cb1
Diogo Geraldo <diogojunqueirageraldo@gmail.com>
2025-03-16 19:38:15 -0300

    first commit

---
+++ src/test/java/com/bytecode/authorizer_data/AuthorizerDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytecode.authorizer_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizerDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
